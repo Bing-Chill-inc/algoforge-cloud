59d9fb31f1c7ba8b53846d25b090fa344301670d
Fetnmax <maximejacob4000@gmail.com>
2025-03-26 08:50:30 +0100

    truc inutile enlevé

--- src/components/auth/LoginForm.svelte
+++ src/components/auth/LoginForm.svelte
@@ -1,22 +1,11 @@
 <script lang="ts">
-	import userStore, { setUser } from "../../stores/userStores";
+	import { setUser } from "../../stores/userStores";
 	import InputField from "../common/InputField.svelte";
 	import Checkbox from "../common/Checkbox.svelte";
 	import Button from "../common/Button.svelte";
 	import Link from "../common/Link.svelte";
-	import { onMount } from "svelte";
-	import { get } from "svelte/store";
 	import { notifications } from "../../stores/notificationStore";
 	import type { User } from "../../utils/types";
-
-	// Rediriger un utilisateur connecté
-	onMount(() => {
-		const user = get(userStore);
-		if (user) {
-			// Rediriger vers la page par défaut (par exemple, le tableau de bord)
-			window.location.hash = "#/";
-		}
-	});
 
 	let email: string = "";
 	let password: string = "";
